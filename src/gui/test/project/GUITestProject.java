b88a0a59ad69b5426e6b15853f55d5009b7e32e7
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-09 16:23:11 +0100

    Adding button and text

--- src/gui/test/project/GUITestProject.java
+++ src/gui/test/project/GUITestProject.java
@@ -9,10 +9,12 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -54,6 +56,23 @@ public class GUITestProject extends Application {
         grid.add(userTextField, 1, 1);
         grid.add(pw, 0, 2);
         grid.add(pwBox, 1, 2);
+        
+        grid.setGridLinesVisible(true);
+        
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn,1,4);
+        
+        final Text actiontarget = new Text();
+        grid.add(actiontarget,1,6);
+        
+        btn.setOnAction(event ->
+        {
+            actiontarget.setFill(Color.FIREBRICK);
+            actiontarget.setText("Sign in button pressed");
+        });
         
         
         

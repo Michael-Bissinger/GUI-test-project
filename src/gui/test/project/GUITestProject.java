4f79147dd4adfd0aedb0317bcc7fccb19bc005cb
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-09 15:52:56 +0100

    Adding elements, that were defined to the grid.

--- src/gui/test/project/GUITestProject.java
+++ src/gui/test/project/GUITestProject.java
@@ -8,7 +8,14 @@ package gui.test.project;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -27,6 +34,28 @@ public class GUITestProject extends Application {
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets (25,25,25,25));
+        
+        Scene scene = new Scene(grid, 300, 275);
+        
+        
+        // Labels and textfields
+        Text scenetitle = new Text("Please Login: ");
+        scenetitle.setFont(Font.font("Tahoma",FontWeight.NORMAL, 20));
+        Label userName = new Label("User Name:");
+        TextField userTextField = new TextField();
+        Label pw = new Label("Password: ");
+        PasswordField pwBox = new PasswordField();
+        
+        
+        // Adding the defined elements to the Grid
+        // The information behind sets the position
+        grid.add(scenetitle,0,0,2,1); //span 2 columns, span 1 row
+        grid.add(userName, 0, 1);
+        grid.add(userTextField, 1, 1);
+        grid.add(pw, 0, 2);
+        grid.add(pwBox, 1, 2);
+        
+        
         
         primaryStage.setTitle("JavaFX Login Form!");
         primaryStage.setScene(scene);

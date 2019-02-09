6a7c89a5d6f62dc31137ba6f69595c88e81d98f0
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-09 15:34:58 +0100

    Prepariing code for Logi

--- src/gui/test/project/GUITestProject.java
+++ src/gui/test/project/GUITestProject.java
@@ -21,28 +21,8 @@ public class GUITestProject extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-//            @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("Hello World!");
-//            }
-//        });
-   
-        //Lambda expression (does the same like the commented out stuff above)
-        btn.setOnAction(event->
-                System.out.println("Hello world!"));
-        
-        
-        StackPane root = new StackPane();
-        //Adding the only existing childnode to our root:
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+         
+        primaryStage.setTitle("JavaFX Login Form!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }

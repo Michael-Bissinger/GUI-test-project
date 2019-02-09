4fcc8e2d341189f4f1e1bbe4df9d7790eaabdc77
Michael Bissinger <Michael.Bissinger@inb.de>
2019-02-09 15:01:38 +0100

    Exchange Hello world with Hello world-lambda expression

--- src/gui/test/project/GUITestProject.java
+++ src/gui/test/project/GUITestProject.java
@@ -23,13 +23,17 @@ public class GUITestProject extends Application {
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
             
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+        
+        btn.setOnAction(event->
+                System.out.println("Hello world!"));
+        
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
